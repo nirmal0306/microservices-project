5f95c4c6da45f051154a3247fa48bc3610a62d69
nirmal <nirmalbarot067@gmail.com>
2025-08-24 12:28:29 +0530

    Initial commit - Dealer Evaluation Project

--- product_service/app.py
+++ product_service/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)  # Enable CORS so frontend can fetch
 
 products = [
     {"id": 1, "name": "Laptop"},
@@ -8,9 +10,10 @@ products = [
     {"id": 3, "name": "Tablet"}
 ]
 
+@app.route('/')
+def home():
+    return "Product Service is running!"
+
 @app.route('/products', methods=['GET'])
 def get_products():
     return jsonify(products)
-
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
